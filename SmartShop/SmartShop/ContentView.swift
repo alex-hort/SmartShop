574006412ff1d08f3a88f596cdcf60d2f0bb1c50
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-12-09 13:58:13 -0600

    Firts commit: iOS UI + backend

---
+++ SmartShop/SmartShop/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SmartShop
+//
+//  Created by Alexis Horteales Espinosa on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartShop/SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by Alexis Horteales Espinosa on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
