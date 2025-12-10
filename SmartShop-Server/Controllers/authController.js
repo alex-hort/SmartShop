d54eebe7b4a074e849db9e174f5024710fd4e272
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-12-10 17:17:16 -0600

    6. Authentocating the user and returning the token

--- SmartShop-Server/Controllers/authController.js
+++ SmartShop-Server/Controllers/authController.js
@@ -1,13 +1,65 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const models = require('../models')
-const { Op} = require('sequelize')
+const { Op, where} = require('sequelize')
 const bcrypt = require('bcrypt')
 const {validationResult} = require('express-validator');
 
 
-exports.register = async (req, res) => {
-    const errors = validationResult(req)
 
+exports.login = async (req, res) => {
+
+    try {
+    //validate input 
+      const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        const msg = errors.array().map(err => err.msg).join('')
+        return res.status(422).json({success: false, message: msg})
+    }
+
+    const { username, password } = req.body
+
+    //check if user exists
+
+    const existinUser = await models.User.findOne({
+        where:{
+            username: {[Op.iLike]: username}
+        }
+        })
+
+        if (!existinUser){
+            return res.status(401).json({message: 'invalid credentials', success: false});
+
+        }
+        ///check the password
+       const isPasswordValid = await bcrypt.compare(password, existinUser.password)
+
+       if(!isPasswordValid){
+        return res.status(401).json({message: 'invalid credentials', success: false});
+       }
+
+       //if the password is valid
+       //generate a token (skipped for now)
+       const token = jwt.sign({userId: existinUser.id}, 'SECRETKEY', {
+        expiresIn: '1h'
+       })
+
+       return res.status(200).json({userId: existinUser.id, username: existinUser.username, token: token, success: true})
+
+    }catch(err){
+
+        res.status(500).json({ message: 'internal server error',success: false})
+
+    }
+
+
+}
+
+
+exports.register = async (req, res) => {
+
+
+    const errors = validationResult(req)
     if(!errors.isEmpty()){
         const msg = errors.array().map(err => err.msg).join('')
         return res.status(422).json({success: false, message: msg})
--- SmartShop-Server/Routes/auth.js
+++ SmartShop-Server/Routes/auth.js
@@ -8,9 +8,13 @@ const registerValidator = [
      body('password', 'password cannot be empty').not().isEmpty(),
 ]
 
+const loginValidator = [
+    body('username', 'username cannot be empty').not().isEmpty(),
+     body('password', 'password cannot be empty').not().isEmpty(),
+]
 
 router.post('/register', registerValidator, authController.register);
-
+router.post('/login', loginValidator, authController.login);
 
 module.exports = router;
 
